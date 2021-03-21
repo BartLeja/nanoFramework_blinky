eb55ba3c7f79fd90bf7e58f1523857fe125b4536
bleja <bartlomiejleja@gmail.com>
2021-03-21 20:01:24 +0100

    First commit

---
+++ Blinky/Program.cs
@@ -0,0 +1,38 @@
+﻿//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+
+using System.Device.Gpio;
+using System.Threading;
+
+namespace Blinky
+{
+	public class Program
+    {
+        private static GpioController s_GpioController;
+        public static void Main()
+        {
+            s_GpioController = new GpioController();
+
+            // ESP32 DevKit: 4 is a valid GPIO pin in, some boards like Xiuxin ESP32 may require GPIO Pin 2 instead.
+            GpioPin led = s_GpioController.OpenPin(
+            2,
+            PinMode.Output);
+
+            led.Write(PinValue.Low);
+
+            while (true)
+            {
+                led.Toggle();
+                Thread.Sleep(125);
+                led.Toggle();
+                Thread.Sleep(125);
+                led.Toggle();
+                Thread.Sleep(125);
+                led.Toggle();
+                Thread.Sleep(525);
+            }
+        }
+    }
+}
